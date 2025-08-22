8a8ca40af8169f4593c07390a5a9bc79c4ffb5af
Praveen Jha <praveenworkforum@gmail.com>
2025-08-22 22:29:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import express from 'express'
+import userRouter from './routes/user.routes.js'
+import urlRouter from './routes/url.routes.js'
+import {authenticationMiddleware} from './middlewares/auth.middleware.js'
+const app = express();
+const PORT = process.env.PORT ?? 8000;
+
+app.use(express.json());
+app.use(authenticationMiddleware);
+
+app.get('/', (req, res) => {
+
+    return res.json({ status: 'server is running ' });
+});
+
+app.use(urlRouter);
+
+app.use('/user',userRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+});
